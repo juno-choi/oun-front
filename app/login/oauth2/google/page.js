4d71ac7b0fa321c5bd31ae4f3dde09e2d8eec571
juno-choi <ililil9482@naver.com>
2025-02-13 11:01:44 +0900

    feat: google code 값 서버로 보내기

--- app/login/oauth2/google/page.js
+++ app/login/oauth2/google/page.js
@@ -8,16 +8,33 @@ export default function GoogleAuthCallback() {
     const fetchToken = async () => {
       const urlParams = new URLSearchParams(window.location.search);
       const code = urlParams.get("code");
-    
+      
       if (!code) {
         console.error("Google OAuth 인가 코드 없음");
         return;
       }
+
+      const body = {
+        "code": code,
+      }
+
+      console.log(body);
+
+      const response = await fetch("http://localhost:8080/api/user/auth/google", {
+        method: "POST",
+        body: JSON.stringify(body),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      console.log(response);
+
       setCode(code);
     };
 
     fetchToken();
   }, []);
 
-  return <div>Google 로그인 처리 중... 인가 코드 = {code}</div>;
+  return <div>Google 로그인 처리 중...</div>;
 }
