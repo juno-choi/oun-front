8582cc9c27b071aafd5d1fdbf3cd4a781a17324b
juno-choi <ililil9482@naver.com>
2025-03-04 09:22:57 +0900

    fix: 이미지 투명하게 변경

--- app/page.js
+++ app/page.js
@@ -1,5 +1,5 @@
 "use client";
-import { useState, useEffect } from 'react';
+
 import MainImage from "@/app/components/main/MainImage";
 import LoginButton from "@/app/components/auth/LoginButton";
 import LogoutButton from "@/app/components/auth/LogoutButton";
@@ -10,8 +10,12 @@ export default function Home() {
 
   return (
     <main className="prose dark:prose-invert flex min-h-screen flex-col items-center justify-center p-5">
-      <MainImage />
-      {isLoggedIn ? <LogoutButton /> : <LoginButton />}
+      <div className="w-full max-w-2xl p-8 rounded-xl bg-white dark:bg-gray-800 shadow-lg dark:shadow-gray-900/30 transition-all duration-300">
+        <MainImage />
+        <div className="mt-8 text-center text-gray-800 dark:text-gray-200">
+          {isLoggedIn ? <LogoutButton /> : <LoginButton />}
+        </div>
+      </div>
     </main>
   );
 }
