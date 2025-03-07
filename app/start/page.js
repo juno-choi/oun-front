aee3d08ccb06e9839b7fe028c0dd4c64596af4fc
juno-choi <ililil9482@naver.com>
2025-03-07 09:15:35 +0900

    feat: start에 로고 추가

--- app/start/page.js
+++ app/start/page.js
@@ -1,12 +1,14 @@
 "use client";
-import withAuth from '../components/auth/withAuth';
-import RoutineGetButton from '../components/routine/RoutineListMoveButton';
-import RoutineCreateMoveButton from '../components/routine/RoutineCreateMoveButton';
-import LogoutButton from '../components/auth/LogoutButton';
+import withAuth from '@/app/components/auth/withAuth';
+import RoutineGetButton from '@/app/components/routine/RoutineListMoveButton';
+import RoutineCreateMoveButton from '@/app/components/routine/RoutineCreateMoveButton';
+import LogoutButton from '@/app/components/auth/LogoutButton';
+import MainLogo from '@/app/components/main/MainLogo';
 
 function StartPage() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen gap-6">
+      <MainLogo />
       <RoutineGetButton />
       <RoutineCreateMoveButton />
       <LogoutButton />
