61e0164d9fbfab6b1785dc7d456d81db0a65e121
juno-choi <ililil9482@naver.com>
2025-03-08 20:12:33 +0900

    fix: 뒤로가기 버튼 fixed로 설정

--- app/components/util/BackHistoryButton.js
+++ app/components/util/BackHistoryButton.js
@@ -4,25 +4,29 @@ import { useRouter } from 'next/navigation';
 export default function BackHistoryButton() {
   const router = useRouter();
 
+
   return (
-    <button
-      onClick={() => router.back()}
-      className="absolute top-8 left-8 p-2 hover:bg-black rounded-full transition-colors duration-200"
-      aria-label="뒤로 가기"
-    >
-      <svg 
-        xmlns="http://www.w3.org/2000/svg" 
-        width="24" 
-        height="24" 
-        viewBox="0 0 24 24" 
-        fill="none" 
-        stroke="currentColor" 
-        strokeWidth="2" 
-        strokeLinecap="round" 
-        strokeLinejoin="round"
+    // 버튼이 항상 위에 있도록 설정
+    <div className="fixed">
+      <button
+        onClick={() => router.back()}
+        className="absolute top-8 left-8 p-2 hover:bg-black rounded-full transition-colors duration-200"
+        aria-label="뒤로 가기"
       >
-        <path d="M19 12H5M12 19l-7-7 7-7"/>
-      </svg>
-    </button>
+        <svg 
+          xmlns="http://www.w3.org/2000/svg" 
+          width="24" 
+          height="24" 
+          viewBox="0 0 24 24" 
+          fill="none" 
+          stroke="currentColor" 
+          strokeWidth="2" 
+          strokeLinecap="round" 
+          strokeLinejoin="round"
+        >
+          <path d="M19 12H5M12 19l-7-7 7-7"/>
+        </svg>
+      </button>
+    </div>
   );
 }
