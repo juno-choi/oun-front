91ff6c37b61d0acaab13fb06ab85b77a8cceaf81
juno-choi <ililil9482@naver.com>
2025-03-12 20:21:48 +0900

    chore: 세트 추가 체크 문구 표시

--- app/components/routine/health/HealthCheckSpan.js
+++ app/components/routine/health/HealthCheckSpan.js
@@ -4,7 +4,7 @@ export default function HealthCheckSpan({health_set_list}) {
             <div className="flex flex-row justify-between items-center">
                 {console.log(health_set_list)}
                 {health_set_list.length === 0 && (
-                    <span className="text-sm">세트를 추가해 주세요 👆</span>
+                    <span className="text-sm text-red-500">세트를 추가해 주세요 👆</span>
                 )}
             </div>
         </div>
