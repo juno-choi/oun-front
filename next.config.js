3cee826488271c556ec1aab732b56f371c955f9b
juno-choi <ililil9482@naver.com>
2025-03-17 23:04:13 +0900

    fix: test 서버 확인을 위해 강제로 데이터 변경

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    NEXT_PUBLIC_API_URL: 'http://localhost',
+    NEXT_PUBLIC_API_URL: 'http://simol.iptime.org',
   },
 }
 
